b053970e84a41a2e3e09ba90cb36dffc00d4e19f
Diogo Silva <dvsdevx@gmail.com>
2022-11-25 11:01:59 -0300

    inserindo estilizacao tailwind css

--- src/App.vue
+++ src/App.vue
@@ -43,11 +43,6 @@ import HeaderComponent from '@/components/HeaderComponent.vue'
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+      "./public/index.html",
       "./src/**/*.{html,js}",
-      "./public/index.html"          
+                
     ],
     theme: {
       extend: {},
