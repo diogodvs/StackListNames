120b9cd83aa4cc955d5c5d15bbe9562e635c1188
Diogo Silva <dvsdevx@gmail.com>
2022-11-25 10:25:59 -0300

    Iniciando projeto acompanhando aula StackX Web 3.0

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderComponent></HeaderComponent>
+ <div class="h-screen">
+  <div class="max-w-[90rem] my-0 mx-auto px-24 py-20">
+    <h1 class="font-semibold text-3xl text-white-600">
+      Lista dos Alunos do Curso
+    </h1>
+    <div class="flex justify-between items-center mt-10">
+        <div class="flex flex-col w-[48%]">
+            <label class="font-semibold text-base text-grey-300">
+              Pesquisar
+            </label>
+            <input
+              type="text" 
+              placeholder="Nome do Aluno"
+              class="bg-dark-400 rounded-lg mt-2 border-none py-3 px-5 text-white-600 text-base"
+            />
+            <label class="font-semibold text-base text-grey-300">
+              Nacionalidade
+            </label>
+            <select
+              name="nacionalidade"
+              id="nacionalidade"
+              class="bg-dark-400 rounded-lg mt-2 border-none py-3 px-5 text-white-600 text-base hover:cursor-pointer"
+              >
+              <option value="" >Todas</option>
+            </select>
+        </div>
+    </div>
+  </div>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import HeaderComponent from '@/components/HeaderComponent.vue'
+  export default {
+    name: 'App',
+    components: {
+      HeaderComponent
+    }
   }
-}
 </script>
 
 <style>
